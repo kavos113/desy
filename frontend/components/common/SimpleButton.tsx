ebcf053e039401e7d387ddccf9ebc490795eebab
kavos113 <kohei.hadano@gmail.com>
2025-10-28 10:17:05 +0900

    button

---
+++ frontend/components/common/SimpleButton.tsx
@@ -0,0 +1,15 @@
+import "./common.css";
+
+type SimpleButtonProps = {
+  text: string;
+};
+
+const SimpleButton = ({ text }: SimpleButtonProps) => {
+  return (
+    <button type="button" className="simple-button">
+      <span className="simple-button-text">{text}</span>
+    </button>
+  );
+};
+
+export default SimpleButton;
