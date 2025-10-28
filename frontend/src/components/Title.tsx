eabfe7f2c5664570249eb2d1b769425fe1fd0ddb
kavos113 <kohei.hadano@gmail.com>
2025-10-28 10:13:53 +0900

    rev

--- frontend/src/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-const Title = () => {
-  return (
-    <div className="title-container">
-      <p className="title">Pocket Syllabus</p>
-    </div>
-  );
-};
-
-export default Title;
--- frontend/src/components/common/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CheckBox } from "./CheckBox";
-export { default as ComboBox } from "./ComboBox";
-export { default as ComboBoxMenu } from "./ComboBoxMenu";
-export { default as SearchBox } from "./SearchBox";
-export { default as SimpleButton } from "./SimpleButton";
-export type { Menu } from "./types";
